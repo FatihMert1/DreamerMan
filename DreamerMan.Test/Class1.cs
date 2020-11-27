39265c5aaddefdcb9d900f53bacd9cef7093a566
Fatih Rahman Mert <fatihmert3858@gmail.com>
2020-11-27 23:56:34 +0300

    Upgrade Workflow File-2

--- DreamerMan.Test/Class1.cs
+++ DreamerMan.Test/Class1.cs
@@ -1,8 +1,16 @@
 ﻿using System;
+using Xunit;
 
 namespace DreamerMan.Test
 {
-    public class Class1
+    public class MathTest
     {
+
+        [Fact]
+        public void Test_Example(){
+            int expected = 10;
+
+            Assert.Equal(expected, 4+6 );
+        }
     }
 }
