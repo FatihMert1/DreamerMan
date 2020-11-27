58812127c4d817b53050d76b9ab076a7758c3282
Fatih Rahman Mert <fatihmert3858@gmail.com>
2020-11-28 01:08:19 +0300

    Tag Example

--- DreamerMan.Test/Class1.cs
+++ DreamerMan.Test/Class1.cs
@@ -10,7 +10,7 @@ namespace DreamerMan.Test
         public void Test_Example(){
             int expected = 10;
 
-            Assert.Equal(expected, 4+6);
+            Assert.Equal(expected, 5+5);
         }
     }
 }
